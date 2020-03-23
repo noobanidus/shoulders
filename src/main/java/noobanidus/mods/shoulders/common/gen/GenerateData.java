5f1f736e29ea4ac936b47e8671c1d8d95a422df2
Jon McManus <due@wxwhatever.com>
2020-03-23 19:31:33 +1000

    Strip out the language, it's not needed.

--- src/main/java/noobanidus/mods/shoulders/common/gen/GenerateData.java
+++ src/main/java/noobanidus/mods/shoulders/common/gen/GenerateData.java
@@ -1,6 +1,5 @@
 package noobanidus.mods.shoulders.common.gen;
 
-import net.minecraftforge.common.data.LanguageProvider;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
@@ -11,12 +10,5 @@ public class GenerateData {
   @SubscribeEvent
   public static void onGatherData(GatherDataEvent event) {
     event.getGenerator().addProvider(new GenerateInfo(event.getGenerator()));
-    event.getGenerator().addProvider(new LanguageProvider(event.getGenerator(), "shoulders", "en_us") {
-      @Override
-      protected void addTranslations() {
-        add("command.shoulders.reload.success", "Successfully reloaded Shoulders information!");
-        add("command.shoulders.reload.failure", "An error occurred while reloading Shoulders information. Please see the log for more details.");
-      }
-    });
   }
 }
