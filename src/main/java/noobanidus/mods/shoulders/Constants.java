fc5c9aa10c695eb8957e5c690475b9a279a209f9
Jon McManus <due@wxwhatever.com>
2022-02-09 21:29:56 +1000

    Update shoulders info.

--- src/main/java/noobanidus/mods/shoulders/Constants.java
+++ src/main/java/noobanidus/mods/shoulders/Constants.java
@@ -7,7 +7,7 @@ import org.apache.logging.log4j.Logger;
 public interface Constants {
   Logger LOG = LogManager.getLogger();
   String MODID = "shoulders";
-  String shoulderUrl = "https://raw.githubusercontent.com/noobanidus/shoulders/1.14/src/generated/resources/data/info/shoulders.json";
+  String shoulderUrl = "https://raw.githubusercontent.com/noobanidus/shoulders/1.16.5/src/generated/resources/data/info/shoulders.json";
 
   static ResourceLocation rl (String path) {
     return new ResourceLocation(MODID, path);
