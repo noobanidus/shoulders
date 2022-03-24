7b0a5aa740c4821e48a34f385a2858724a6cf946
Jon McManus <due@wxwhatever.com>
2022-03-25 09:54:31 +1000

    Remove jerboa for now.
    This will cause previous versions of Shoulders to crash.

--- src/main/java/noobanidus/libs/shoulders/gen/GenerateInfo.java
+++ src/main/java/noobanidus/libs/shoulders/gen/GenerateInfo.java
@@ -40,7 +40,7 @@ public class GenerateInfo /*implements IDataProvider*/ {
         new ShoulderData(UUID.fromString("e5e2b89d-3884-4190-8b69-97863eb95bc6"), ShoulderEntity.FROG, Shoulder.LEFT, 0), // katubug
         new ShoulderData(UUID.fromString("e3298bd7-61bf-427f-af89-4e418a20bf57"), ShoulderEntity.FOX, Shoulder.RIGHT, 1), // ChosenArchitect
 new ShoulderData(UUID.fromString("59199808-a261-42dc-aff9-6df568615020"), ShoulderEntity.SPROUT, Shoulder.RIGHT, 0), // CosmicParticle
-        new ShoulderData(UUID.fromString("564267c7-2ad2-4059-866a-6ca980b32777"), ShoulderEntity.JERBOA, Shoulder.RIGHT, 3) // VallenFrostweavr
+/*        new ShoulderData(UUID.fromString("564267c7-2ad2-4059-866a-6ca980b32777"), ShoulderEntity.JERBOA, Shoulder.RIGHT, 3) // VallenFrostweavr*/
     );
 
     JsonArray dataArray = new JsonArray();
